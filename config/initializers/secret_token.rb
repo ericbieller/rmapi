4b808923fa60555dd21d83775321da741faf7859
Eric Bieller <ericbieller@gmail.com>
2013-03-02 13:14:30 -0800

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmapi::Application.config.secret_token = '3ab6a8e4fbd3a03a44feafa8bd089cef7eb5da49d696b60f5b8fbdaab9d750896a8f9976e828c3eef5fc51611868229f154ef8ba60f6e700fc202e796ad7127f'

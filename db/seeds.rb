9e7473fa6adb87cc0319396a18860426f24b165b
Nikita Makeyev <nikita@nikitamakeyev.com>
2013-03-03 20:44:56 -0800

    fixed country seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
-CSV.read('/Users/ericbieller/countries_seed.txt').each do |row|
+CSV.read('/Users/nikita/Sites/rmapi/app/countries_seed.txt').each do |row|
   Country.create(
     :name => row[0],
     :code => row[1],
@@ -16,13 +16,13 @@ CSV.read('/Users/ericbieller/countries_seed.txt').each do |row|
     :qualifies_for_royal_mail_airsure => row[5],
     :qualifies_for_royal_mail_international_signed_for => row[6])
 end
-CSV.read('/Users/ericbieller/country_zones.txt').each do |row|
+CSV.read('/Users/nikita/Sites/rmapi/app/country_zones.txt').each do |row|
   ShippingService.create(
     :id => row[0],
     :name => row[1]
     )
 end
-CSV.read('/Users/ericbieller/shipping_rates.txt').each do |row|
+CSV.read('/Users/nikita/Sites/rmapi/app/shipping_rates.txt').each do |row|
   ShippingRate.create(
     :id => row[0],
     :service_id => row[1],
